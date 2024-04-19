5dc50935f621f2270e97c016d4cd46dcd51af239
eklairs <eklairs@proton.me>
2024-04-19 17:54:09 +0530

    tlock: Setup bubbletea

---
+++ models/root.go
@@ -0,0 +1,44 @@
+package models
+
+import (
+    tea "github.com/charmbracelet/bubbletea"
+)
+
+// Root Model
+type RootModel struct {
+
+}
+
+// Initialize root model
+func InitializeRootModel() RootModel {
+    return RootModel {
+
+    }
+}
+
+// Init
+func (m RootModel) Init() tea.Cmd {
+    return nil
+}
+
+// Update
+func (m RootModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+    // Cmds
+	var cmds []tea.Cmd = make([]tea.Cmd, 0)
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+        switch msg.String() {
+        case "q":
+            cmds = append(cmds, tea.Quit)
+        }
+	}
+
+	return m, tea.Batch(cmds...)
+}
+
+// View
+func (m RootModel) View() string {
+    return ""
+}
+
--- tlock/main.go
+++ tlock/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/eklairs/tlock/models"
+)
+
+// TLock go brrr
+func main() {
+    program := tea.NewProgram(models.InitializeRootModel(), tea.WithAltScreen());
+
+    if _, err := program.Run(); err != nil {
+        log.Fatalf("Error while running tlock: %v", err)
+    }
 }
+
