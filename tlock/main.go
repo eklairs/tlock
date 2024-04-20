5612968a79689156b71ba3c039ec6fec048c3e90
eklairs <eklairs@proton.me>
2024-04-20 09:59:59 +0530

    tlock: Setup logging to local home directory

--- tlock/main.go
+++ tlock/main.go
@@ -2,15 +2,45 @@ package main
 
 import (
 	"log"
+	"os"
+	"path"
 
+	"github.com/adrg/xdg"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/eklairs/tlock/models"
 )
 
-// TLock go brrr
+// Path to the log file
+var LOG_DIR = path.Join(xdg.DataHome, "tlock", "logs")
+
+// Sets up logging to file
+func setup_logging() *os.File {
+    // Make log dir
+    os.MkdirAll(LOG_DIR, os.ModePerm)
+
+    // Log to file
+    file, err := tea.LogToFile(path.Join(LOG_DIR, "tlock.log"), "tlock");
+
+    if err != nil {
+        log.Fatalf("Failed to create log file at %s", LOG_DIR)
+    }
+
+    // Return
+    return file
+}
+
+// TLock go brrrrr
 func main() {
+    // Setup logging to file
+    defer setup_logging().Close()
+
+    // Log
+    log.Printf("[tlock] Starting tlock!")
+
+    // Initialize tea program
     program := tea.NewProgram(models.InitializeRootModel(), tea.WithAltScreen());
 
+    // Start tea program
     if _, err := program.Run(); err != nil {
         log.Fatalf("Error while running tlock: %v", err)
     }
