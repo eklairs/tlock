ad9f2400f0356200767aebdf6a85981ccb2d5a7a
eklairs <eklairs@proton.me>
2024-04-24 10:51:36 +0530

    tlock: styles: Add base styles

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/eklairs/tlock
+
+go 1.22.2
+
+require github.com/charmbracelet/lipgloss v0.10.0
+
+require (
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+)
---
+++ tlock-styles/styles.go
@@ -0,0 +1,67 @@
+package tlockstyles
+
+import (
+	"github.com/charmbracelet/lipgloss"
+)
+
+// Instance of the styles
+// Must call `InitializeStyles()` to initialize these styles
+var Styles TLockStyles
+
+// Represents a theme
+type Theme struct {
+	// Background color
+	Background lipgloss.Color
+
+	// Main Color
+	Accent lipgloss.Color
+
+	// Sub color
+	Sub lipgloss.Color
+
+	// Sub Alt color
+	SubAlt lipgloss.Color
+
+	// Text color
+	Text lipgloss.Color
+
+	// Error
+	Error lipgloss.Color
+}
+
+// Styles used by tlock
+type TLockStyles struct {
+	// Title
+	Title lipgloss.Style
+
+	// SubText
+	SubText lipgloss.Style
+
+	// Style for active list items
+	ListItemActive lipgloss.Style
+
+	// Style for inactive list item
+	ListItemInactive lipgloss.Style
+}
+
+// Initializes the styles
+func InitializeStyles(theme Theme) {
+	// Base that every style must copy from
+	base := lipgloss.NewStyle()
+
+	// Base for padded items
+	paddedItem := with(base).Padding(1, 3)
+
+	// Initialize styles
+	Styles = TLockStyles{
+		Title:            with(base).Foreground(theme.Accent),
+		SubText:          with(base).Foreground(theme.Sub),
+		ListItemActive:   with(paddedItem).Background(theme.SubAlt),
+		ListItemInactive: with(paddedItem),
+	}
+}
+
+// Utility to copy a style
+func with(style lipgloss.Style) lipgloss.Style {
+	return style.Copy()
+}
