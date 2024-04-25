c922e9971da39dd31cf8b4e1ff3fd89736e5d86c
eklairs <eklairs@proton.me>
2024-04-25 17:40:48 +0530

    tlock: Use my own fork of `bubbletea`

--- go.mod
+++ go.mod
@@ -23,7 +23,7 @@ require (
 	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/boombuler/barcode v1.0.1-0.20190219062509-6c824513bacc // indirect
-	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
+	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/gen2brain/shm v0.0.0-20230802011745-f2460f5984f7 // indirect
 	github.com/jezek/xgb v1.1.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
@@ -31,7 +31,7 @@ require (
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
+	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
@@ -39,8 +39,10 @@ require (
 	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
 	golang.org/x/image v0.10.0 // indirect
 	golang.org/x/mobile v0.0.0-20230301163155-e0f57694e12c // indirect
-	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
+
+replace github.com/charmbracelet/bubbletea => ./bubbletea/
--- tlock/main.go
+++ tlock/main.go
@@ -2,46 +2,23 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
-	"os"
 
 	"github.com/eklairs/tlock/tlock-internal/context"
 	tlockstyles "github.com/eklairs/tlock/tlock-styles"
 	"github.com/muesli/termenv"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	tlockmodels "github.com/eklairs/tlock/tlock-models"
 )
 
-// Patches the stdout based on the context
-// Patching basically changes the background color to the theme's Background color
-func PatchOutput(context context.Context) (termenv.Color, io.Writer) {
-	r, g, b, _ := context.GetCurrentTheme().Background.RGBA()
-
-	// New termenv output
-	output := termenv.NewOutput(os.Stdout, termenv.WithProfile(termenv.TrueColor))
-
-	// Save background color
-	original_bg := output.BackgroundColor()
+// Changes lipgloss.color to termenv.Color
+func ToTermenvColor(color lipgloss.Color) termenv.Color {
+	r, g, b, _ := color.RGBA()
 
 	// Set background color
-	output.SetBackgroundColor(output.Color(fmt.Sprintf("#%02x%02x%02x", r, g, b)))
-
-	// Return
-	return original_bg, output
-}
-
-// Undos the patch and bring backs the original color
-func ClearOutput(original_bg termenv.Color, output io.Writer) {
-	term_output, ok := output.(*termenv.Output)
-
-	if !ok {
-		panic("Invalid output recieved - expected a termenv output")
-	}
-
-	// Reset
-	term_output.SetBackgroundColor(original_bg)
+	return termenv.RGBColor(fmt.Sprintf("#%02x%02x%02x", r, g, b))
 }
 
 // TLock go brrr
@@ -52,17 +29,11 @@ func main() {
 	// Initialize styles
 	tlockstyles.InitializeStyles(context.GetCurrentTheme())
 
-	// Patch stdout
-	original_bg, output := PatchOutput(context)
-
 	// Start program
-	program := tea.NewProgram(tlockmodels.InitializeRootModel(context), tea.WithAltScreen(), tea.WithOutput(output))
+	program := tea.NewProgram(tlockmodels.InitializeRootModel(context), tea.WithAltScreen(), tea.WithBackgroundColor(ToTermenvColor(context.GetCurrentTheme().Background)))
 
 	// Run
 	if _, err := program.Run(); err != nil {
 		log.Fatalf("[tlock] Error while running app: %v", err)
 	}
-
-	// Clear
-	ClearOutput(original_bg, output)
 }
