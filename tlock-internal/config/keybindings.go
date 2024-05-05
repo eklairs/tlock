20e11ddb354632794df454b660b722ee8155b4e3
eklairs <eklairs@proton.me>
2024-05-05 12:04:02 +0530

    tlock: Add impl to bind token keybindings to config

--- tlock-internal/config/keybindings.go
+++ tlock-internal/config/keybindings.go
@@ -38,21 +38,25 @@ func new_key(keys ...string) key.Binding {
 type KeybindingsConfig struct {
 	// Folder keybindings
 	Folder FolderKeyBinds
+
+	// Token keybindings
+	Tokens TokenKeyBinds
 }
 
 // / Returns the default keybindings
 func DefaultKeybindingsConfig() KeybindingsConfig {
 	return KeybindingsConfig{
 		Folder: DefaultFolderKeyBinds(),
+		Tokens: DefaultTokensKeyBinds(),
 	}
 }
 
 // Folder keybinds
 type FolderKeyBinds struct {
-	// Add token
+	// Add folder
 	Add key.Binding
 
-	// Edit token
+	// Edit folder
 	Edit key.Binding
 
 	// Next
@@ -100,6 +104,79 @@ func ParseFolderKeyBinds(keys map[string][]string) FolderKeyBinds {
 	}
 }
 
+// Tokens keybinds
+type TokenKeyBinds struct {
+	// Add token
+	Add key.Binding
+
+	// Add token from screen
+	AddScreen key.Binding
+
+	// Edit token
+	Edit key.Binding
+
+	// Next
+	Next key.Binding
+
+	// Previous
+	Previous key.Binding
+
+	// Move folder up
+	MoveUp key.Binding
+
+	// Move folder down
+	MoveDown key.Binding
+
+	// Delete
+	Delete key.Binding
+
+	// Delete
+	Copy key.Binding
+
+	// Next token for HOTP
+	NextHOTP key.Binding
+
+	// Next token for HOTP
+	Move key.Binding
+}
+
+// Default tokens keybindings
+func DefaultTokensKeyBinds() TokenKeyBinds {
+	return TokenKeyBinds{
+		Add:       new_key("a"),
+		Edit:      new_key("e"),
+		Next:      new_key("j"),
+		Previous:  new_key("k"),
+		MoveUp:    new_key("J"),
+		MoveDown:  new_key("K"),
+		Delete:    new_key("d"),
+		AddScreen: new_key("s"),
+		Copy:      new_key("c"),
+		Move:      new_key("m"),
+		NextHOTP:  new_key("n"),
+	}
+}
+
+// Parses a map to Folder keybindings
+func ParseTokensKeyBinds(keys map[string][]string) TokenKeyBinds {
+	// Get the default keys
+	default_keys := DefaultTokensKeyBinds()
+
+	return TokenKeyBinds{
+		Add:       new_key(find(keys, "add", default_keys.Add)...),
+		Edit:      new_key(find(keys, "edit", default_keys.Edit)...),
+		Next:      new_key(find(keys, "next", default_keys.Next)...),
+		Previous:  new_key(find(keys, "previous", default_keys.Previous)...),
+		MoveUp:    new_key(find(keys, "move_up", default_keys.MoveUp)...),
+		MoveDown:  new_key(find(keys, "move_down", default_keys.MoveDown)...),
+		Delete:    new_key(find(keys, "delete", default_keys.Delete)...),
+		AddScreen: new_key(find(keys, "add_from_screen", default_keys.AddScreen)...),
+		Copy:      new_key(find(keys, "copy", default_keys.Copy)...),
+		NextHOTP:  new_key(find(keys, "next_hotp", default_keys.NextHOTP)...),
+		Move:      new_key(find(keys, "move", default_keys.Move)...),
+	}
+}
+
 // Load key bindings
 func LoadKeyBindings() KeybindingsConfig {
 	// Read file
@@ -121,5 +198,6 @@ func LoadKeyBindings() KeybindingsConfig {
 	// Keybindings
 	return KeybindingsConfig{
 		Folder: parse(mapped_keys, "folders", ParseFolderKeyBinds, DefaultFolderKeyBinds),
+		Tokens: parse(mapped_keys, "tokens", ParseTokensKeyBinds, DefaultTokensKeyBinds),
 	}
 }
