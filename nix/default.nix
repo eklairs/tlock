77d04356d5344959647f6c09b54b5227ffb3f686
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2024-05-14 23:01:01 +0200

    tlock: build: Add nix flake and derivation

---
+++ nix/default.nix
@@ -0,0 +1,21 @@
+{ pkgs, lib, buildGoModule, git_version, ... }: let
+  manifest = builtins.fromJSON (builtins.readFile (../. + "/tlock.json"));
+in buildGoModule rec {
+  pname = "tlock";
+  version = manifest.version;
+
+  src = lib.cleanSource ../.;
+  vendorHash = "sha256-G402CigSvloF/SI9Wbcts/So1impMUH5kroxDD/KKew=";
+
+  ldflags = [
+    "-X github.com/eklairs/tlock/tlock-internal/constants.VERSION=v${version}-${git_version}"
+  ];
+
+  excludedPackages = [ "bubbletea" ];
+  
+  meta = with lib; {
+    description = "Two-Factor Authentication Tokens Manager in Terminal";
+    homepage = "https://github.com/eklairs/tlock";
+    license = licenses.mit;
+  };
+}
