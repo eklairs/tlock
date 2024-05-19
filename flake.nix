923c94d422234c3882b6b6c4a746fb105c469a63
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2024-05-19 14:48:17 +0200

    tlock: chore: Add devshell to nix flake and a .envrc to load it with direnv

--- flake.nix
+++ flake.nix
@@ -11,12 +11,15 @@
     perSystem = { system, ... }: let
       pkgs = import nixpkgs { inherit system; };
       git_version = self.shortRev or self.dirtyShortRev;
-    in {
+    in rec {
       packages = rec {
         tlock = pkgs.callPackage ./nix { inherit git_version; };
         default = tlock;
       };
+
+      devShells.default = pkgs.mkShell {
+        inputsFrom = [ packages.default ];
+      };
     };
   };
 }
-
